a952dc516f50dd946d9fbf7d22e282f21b96afd6
Himal Majumder <himalmajumder@gmail.com>
2022-03-30 02:33:39 +0600

    Add_TO_CART and REMOVE_FROM_CART reducer implement

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
+import { CartState } from "../context/Context";
 import Rating from "./Rating";
 
 const SingleProduct = ({ product }) => {
+	const {
+		state: { cart },
+		dispatch,
+	} = CartState();
+
 	return (
 		<div className="products">
 			<Card>
@@ -22,10 +28,31 @@ const SingleProduct = ({ product }) => {
 						)}
 						<Rating rating={product.ratings} />
 					</Card.Subtitle>
-					<Button variant="danger">Remove from Cart</Button>
-					<Button disabled={product.inStock}>
-						{product.inStock ? "Out of Stock" : "Add to Cart"}
-					</Button>
+					{cart.some((p) => p.id === product.id) ? (
+						<Button
+							variant="danger"
+							onClick={() => {
+								dispatch({
+									type: "REMOVE_FROM_CART",
+									payload: product,
+								});
+							}}
+						>
+							Remove from Cart
+						</Button>
+					) : (
+						<Button
+							onClick={() => {
+								dispatch({
+									type: "ADD_TO_CART",
+									payload: product,
+								});
+							}}
+							disabled={product.inStock}
+						>
+							{product.inStock ? "Out of Stock" : "Add to Cart"}
+						</Button>
+					)}
 				</Card.Body>
 			</Card>
 		</div>
--- src/context/Reducer.js
+++ src/context/Reducer.js
@@ -1,6 +1,16 @@
 export const cartReducer = (state, action) => {
 	switch (action.type) {
-		case "increment":
+		case "ADD_TO_CART":
+			return {
+				...state,
+				cart: [...state.cart, { ...action.payload, qty: 1 }],
+			};
+			break;
+		case "REMOVE_FROM_CART":
+			return {
+				...state,
+				cart: state.cart.filter((c) => c.id !== action.payload.id),
+			};
 			break;
 
 		default:
